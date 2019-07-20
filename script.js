38988de3fe1644462bd72350d62f61885f4eb0fe
syedsaqib12 <49809570+syedsaqib12@users.noreply.github.com>
2019-07-20 16:02:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,182 @@
+function names() {
+let intelligenceLevels = [
+
+
+    "you are very awesome",
+    "you are very intelligent",
+    "you are very great",
+    "you are very sweet",
+    "you are very dumb",
+    "you are very learner",
+    "you are very creative",
+    "you are very handsome",
+    "you are very pro-active",
+    "you are very confident",
+    "you are very good developer",
+    "you are very boored"
+
+]
+
+
+
+
+
+
+
+
+let name = document.querySelector("input").value;
+let intelligence;
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "a") {
+        intelligence = intelligenceLevels[6];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "b") {
+        intelligence = intelligenceLevels[5];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "c") {
+        intelligence = intelligenceLevels[5];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "d") {
+        intelligence = intelligenceLevels[4];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "e") {
+        intelligence = intelligenceLevels[3];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "f") {
+        intelligence = intelligenceLevels[2];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "g") {
+        intelligence = intelligenceLevels[1];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "h") {
+        intelligence = intelligenceLevels[0];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "i") {
+        intelligence = intelligenceLevels[7];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "j") {
+        intelligence = intelligenceLevels[8];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.chartAt(0).toLowerCase() == "k") {
+        intelligence = intelligenceLevels[9];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "l") {
+        intelligence = intelligenceLevels[10];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "m") {
+        intelligence = intelligenceLevels[5];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+
+
+if (name == false) {
+    alert("please enter a name");
+} else {
+    if (name.charAt(0).toLowerCase() == "n") {
+        intelligence = intelligenceLevels[6];
+        alert(intelligence);
+    } else if (name.charAt(0).toLow);
+}
+
+}
